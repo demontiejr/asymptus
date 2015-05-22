e461197645a5298e920c5a2823830e00eff7d702
demontiejunior <demontiejunior@gmail.com>
2015-05-22 15:41:28 -0300

    Adding argv size inference.

--- ComplexityInference/RegionAnalysis.cpp
+++ ComplexityInference/RegionAnalysis.cpp
@@ -9,13 +9,13 @@
 #include "RegionAnalysis.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/Support/raw_ostream.h"
-//#include "../../RangeAnalysis/RangeAnalysis.h"
+#include "../RangeAnalysis/RangeAnalysis.h"
 
 RegionAnalysis::RegionAnalysis() : ModulePass(ID) {}
 
 void RegionAnalysis::getAnalysisUsage(AnalysisUsage &AU) const{
     AU.setPreservesAll();
-    //AU.addRequired<IntraProceduralRA<Cousot> >();
+    AU.addRequired<IntraProceduralRA<Cousot> >();
 }
 
 bool RegionAnalysis::runOnModule(Module &M) {
@@ -23,27 +23,36 @@ bool RegionAnalysis::runOnModule(Module &M) {
     if (!F) //error
         return false;
     
-    //IntraProceduralRA<Cousot> &ra = getAnalysis<IntraProceduralRA<Cousot> >(*F);
+    StringRef varName;
+    for (Function::arg_iterator arg = F->arg_begin(); arg != F->arg_end(); arg++) {
+        Type *t = arg->getType();
+        if (t->isPointerTy() && t->getContainedType(0)->isPointerTy()) {
+            varName = arg->getName();
+            break;
+        }
+    }
+
+    IntraProceduralRA<Cousot> &ra = getAnalysis<IntraProceduralRA<Cousot> >(*F);
     
     GetElementPtrInst *GEP;
-    //Range r = Range(Max, Min);
+    Range r = Range(Max, Min);
     for (Function::iterator bb = F->begin(), e = F->end(); bb != e; ++bb) {
         for (BasicBlock::iterator i = bb->begin(), e = bb->end(); i != e; ++i) {
-            if (!(GEP = dyn_cast<GetElementPtrInst>(i)) || GEP->getPointerOperand()->getName() != "argv")
+            if (!(GEP = dyn_cast<GetElementPtrInst>(i)) || GEP->getPointerOperand()->getName() != varName)
                 continue;
             for (GetElementPtrInst::op_iterator index = GEP->idx_begin(), idx_end = GEP->idx_end(); index != idx_end; index++) {
                 Value *v = *index;
                 //if (r)
                 //    r = ra.getRange(v);
                 //else
-                    //r = r.unionWith(ra.getRange(v));
+                    r = r.unionWith(ra.getRange(v));
                 
                 //if (isa<ConstantInt>(*index))
                 //    errs() << "constante\n";
             }
         }
     }
-    //errs() << "Number of arguments: " << r.getUpper() << "\n";
+    errs() << "Number of arguments: " << r.getUpper() << "\n";
     //r.print(errs());
     //errs() << "\n";
     return false;
